0687f36254fa2e5f2481887d6cd41756d1f5e8f1
Steven Mao <steven2003mao@gmail.com>
2022-12-30 22:10:42 -0600

    Preliminary Update. Hiding Navbar Added

--- control.js
+++ control.js
@@ -1,10 +1,26 @@
 let availableToScroll = true;
+let lastScrollPosition = 0;
 
-window.onscroll = (ev) => {
+// All scroll effects
+window.onscroll = () => {
     if (availableToScroll) {
-        let progress = 100 - Math.floor((document.body.scrollTop || document.documentElement.scrollTop) / (document.body.scrollHeight - window.innerHeight) * 100);
+        let position = document.documentElement.scrollTop;
+
+        //hide navigation?
+        let isHidden = $("nav").hasClass("navbar-hidden");
+        if ((position > lastScrollPosition && !isHidden) || (position < lastScrollPosition && isHidden)) {
+            $("nav").toggleClass("navbar-hidden");
+            $(".progress-track").toggleClass("raise-progress-bar");
+            $(".progress-bar").toggleClass("raise-progress-bar");
+        }
+
+        //progress bar
+        let progress = 100 - Math.floor((document.body.scrollTop || position) / (document.body.scrollHeight - window.innerHeight) * 100);
         $(".progress-bar").css("width", progress + "%");
+
+        //putting scroll on cooldown, update lastScrollPosition after done
         availableToScroll = false;
         setTimeout(() => {availableToScroll = true;}, 200);
+        lastScrollPosition = position;
     }
 }
