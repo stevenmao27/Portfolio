d8c8dba3671260eb0f5a679f2681dc400cd90e31
Steven Mao <steven2003mao@gmail.com>
2023-01-03 00:39:29 -0600

    Javascript Typing Animation Loop

--- control.js
+++ control.js
@@ -6,14 +6,14 @@ window.onscroll = () => {
     if (availableToScroll) {
         let position = document.documentElement.scrollTop;
 
-        //hide navigation?
+        //hide or reveal navigation?
         let isHidden = $("nav").hasClass("navbar-hidden");
         if ((position > lastScrollPosition && !isHidden) || (position < lastScrollPosition && isHidden)) {
             $("nav").toggleClass("navbar-hidden");
             $(".progress-track").toggleClass("raise-progress-bar");
         }
 
-        //progress bar
+        //update progress bar
         let progress = 100 * (document.body.scrollTop || position) / (document.body.scrollHeight - window.innerHeight);
         $(".progress-bar").css("transform", `translateX(${progress}vw)`);
 
@@ -21,5 +21,50 @@ window.onscroll = () => {
         availableToScroll = false;
         setTimeout(() => {availableToScroll = true;}, 200);
         lastScrollPosition = position;
+    }
+}
+
+
+
+window.onload = () => {
+    // run this self-running function infinitely
+    // setTimeout(draw, 1000);
+    html = $('.intro-desc')
+    html.css("animation", "blinking 1s step-end infinite")
+}
+
+
+let i = 0; // index of character in current word
+let w = 0; // index of word in word bank
+let wordBank = ["I'm Studying Software Engineering", "I'm Studying Data Science"];
+let currWord = wordBank[w];
+let html; // html object
+let mode = 0; // 0: adding, 1: waiting, 2: deleting
+
+
+//INFINITE RECURSION. DOES EVERYTHING. MODES: add html, do nothing, delete
+function draw() {
+    html.text(html.text() + currWord[i])
+    i++
+    // adding done
+    if (i !== currWord.length) setTimeout(draw, 500)
+    else {
+        i--
+        setTimeout(undraw, 3000)
+    }
+}
+
+function undraw() {
+    html.text(html.text().slice(0, -1))
+    i--
+    // deleting done, set up next word
+    if (i >= 0) {
+        setTimeout(undraw, 500)
+    }
+    else {
+        i = 0
+        w = (w + 1) % wordBank.length
+        currWord = wordBank[w]
+        setTimeout(draw, 1000)
     }
 }
